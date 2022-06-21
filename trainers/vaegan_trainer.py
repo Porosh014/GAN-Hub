c1af68827682739e83dabed81832665a550dca61
HAL9000 <porosh.chowdhurey@gmail.com>
2022-06-21 23:42:22 +0200

    vae-gan updates

--- trainers/vaegan_trainer.py
+++ trainers/vaegan_trainer.py
@@ -56,6 +56,10 @@ class VAEGAN_Train:
         self.lr_discriminator = ExponentialLR(self.optimizer_discriminator, gamma=self.config["decay_lr"])
 
     def _train_one_epoch(self):
+        count = 0
+        mean_encoder_loss = 0
+        mean_decoder_loss = 0
+        mean_discriminator_loss = 0
 
         for i, data in tqdm(enumerate(self.train_dataloader), desc="training-minibatch"):
 
@@ -108,5 +112,93 @@ class VAEGAN_Train:
             # loss_discriminator.backward()  # [p.grad.data.clamp_(-1,1) for p in net.discriminator.parameters()]
             # self.optimizer_discriminator.step()
 
-            print('[%02d] encoder loss: %.5f | decoder loss: %.5f | discriminator loss: %.5f' % (
-            i, loss_encoder, loss_decoder, loss_discriminator))
+            mean_encoder_loss += loss_encoder.item()
+            mean_decoder_loss += loss_decoder.item()
+            mean_discriminator_loss += loss_discriminator.item()
+            count += 1
+
+        mean_encoder_loss = mean_encoder_loss / count
+        mean_decoder_loss = mean_decoder_loss / count
+        mean_discriminator_loss = mean_discriminator_loss / count
+
+        return mean_encoder_loss, mean_decoder_loss, mean_discriminator_loss
+
+    def _validate_one_epoch(self):
+        count = 0
+        mean_encoder_loss = 0
+        mean_decoder_loss = 0
+        mean_discriminator_loss = 0
+
+        with torch.no_grad():
+            for i, data in tqdm(enumerate(self.val_dataloader), desc="validating-minibatch"):
+
+                self.network.eval()
+
+                data = Variable(data, requires_grad=False).float().cuda()
+                x_t, disc_class, disc_layer, mus, log_variances = self.network(data)
+
+                # split so we can get the different parts
+                disc_layer_original = disc_layer[:self.config["batch_size"]]
+                disc_layer_predicted = disc_layer[self.config["batch_size"]:-self.config["batch_size"]]
+                disc_layer_sampled = disc_layer[-self.config["batch_size"]:]
+
+                disc_class_original = disc_class[:self.config["batch_size"]]
+                disc_class_predicted = disc_class[self.config["batch_size"]:-self.config["batch_size"]]
+                disc_class_sampled = disc_class[-self.config["batch_size"]:]
+
+                nle, kl, mse, bce_dis_original, bce_dis_predicted, bce_dis_sampled = VAEGAN.loss(data, x_t,
+                                                                                                 disc_layer_original,
+                                                                                                 disc_layer_predicted,
+                                                                                                 disc_layer_sampled,
+                                                                                                 disc_class_original,
+                                                                                                 disc_class_predicted,
+                                                                                                 disc_class_sampled, mus,
+                                                                                                 log_variances)
+
+                # THIS IS THE MOST IMPORTANT PART OF THE CODE
+                loss_encoder = torch.sum(kl) + torch.sum(mse)
+                loss_discriminator = torch.sum(bce_dis_original) + torch.sum(bce_dis_predicted) + torch.sum(bce_dis_sampled)
+                loss_decoder = torch.sum(self.config["lambda_mse"] * mse) - (
+                            1.0 - self.config["lambda_mse"]) * loss_discriminator
+
+                mean_encoder_loss += loss_encoder.item()
+                mean_decoder_loss += loss_decoder.item()
+                mean_discriminator_loss += loss_discriminator.item()
+                count += 1
+
+        mean_encoder_loss = mean_encoder_loss / count
+        mean_decoder_loss = mean_decoder_loss / count
+        mean_discriminator_loss = mean_discriminator_loss / count
+
+        return mean_encoder_loss, mean_decoder_loss, mean_discriminator_loss
+
+    def write_test_results(self):
+        self.network.eval()
+
+        with torch.no_grad():
+            for i, data in tqdm(enumerate(self.test_dataloader), desc="writing output in disk:"):
+
+                if (i+1) == self.config["num_test_images"]:
+                    break
+
+                else:
+                    data = Variable(data, requires_grad=False).float().cuda()
+                    x_t, _, _, _, _ = self.network(data)
+
+                    save_image(x_t, "output/vae-gan/images/%d.png" % i, nrow=8, normalize=True)
+
+
+    def _train(self):
+        for epoch in range(self.config["epochs"]):
+            train_encoder_loss, train_decoder_loss, train_discriminator_loss = self._train_one_epoch()
+            valid_encoder_loss, valid_decoder_loss, valid_discriminator_loss = self._validate_one_epoch()
+            self.write_test_results()
+
+            print("epoch {}, train-encoder-loss: {}, train-decoder-loss: {}, train-discriminator-loss: {},".format(
+                (epoch + 1), train_encoder_loss, train_decoder_loss, train_discriminator_loss
+            ))
+
+            print("epoch {}, valid-encoder-loss: {}, valid-decoder-loss: {}, valid-discriminator-loss: {},".format(
+                (epoch + 1), valid_encoder_loss, valid_decoder_loss, valid_discriminator_loss
+            ))
+
